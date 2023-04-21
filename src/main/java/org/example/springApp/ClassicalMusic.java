f92adbb34069018a6df94210beb6d0fdd8e52586
upravaD <daktah@icloud.com>
2023-04-21 17:20:22 +0400

    upgrade Bean

--- src/main/java/org/example/springApp/ClassicalMusic.java
+++ src/main/java/org/example/springApp/ClassicalMusic.java
@@ -1,9 +1,31 @@
 package org.example.springApp;
 
 public class ClassicalMusic implements Music {
+    private String song;
+
+    public ClassicalMusic() {
+    }
+
+    public static ClassicalMusic getClassicFactory() {
+        return new ClassicalMusic();
+    }
+
+    public void init() {
+        System.out.println("Initialization " + ClassicalMusic.class.getSimpleName() + " complete!");
+        TestSpring.COUNT++;
+    }
+
+    public void destroy() {
+        System.out.println("Destruction " + ClassicalMusic.class.getSimpleName() + " complete!");
+    }
+
     @Override
     public String getSong() {
-        return "Classic";
+        return song;
+    }
+
+    public void setSong(String song) {
+        this.song = song;
     }
 
     @Override
--- src/main/java/org/example/springApp/TestSpring.java
+++ src/main/java/org/example/springApp/TestSpring.java
@@ -2,21 +2,46 @@ package org.example.springApp;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.Random;
+
 public class TestSpring {
+    public static int COUNT = 0;
+
     public static void main(String[] args) {
+
+        // Создание ApplicationContext из файла xml
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml");
 
-//        Music musicBean = context.getBean("musicBean", Music.class);
-//        MusicPlayer musicPlayer = new MusicPlayer(musicBean);
+        // Создание бинов MusicPlayer через ApplicationContext
+        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        boolean equalsResult = firstMusicPlayer == secondMusicPlayer;
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        // Сравнение бинов MusicPlayer
+        System.out.println("=".repeat(50));
+        System.out.println("equalsResult = " + equalsResult + " {");
+        System.out.println("firstMusicPlayer = " + firstMusicPlayer);
+        System.out.println("secondMusicPlayer = " + secondMusicPlayer);
+        System.out.println("}");
 
-        musicPlayer.playMusic();
+        // Изменение полей класса MusicPlayer
+        System.out.println("=".repeat(50));
+        firstMusicPlayer.setVolume(10);
+        secondMusicPlayer.setName("upravaD");
+        System.out.println("firstMusicPlayer: { " + "name = " + firstMusicPlayer.getName() + "; volume = " + firstMusicPlayer.getVolume() + " }");
+        System.out.println("secondMusicPlayer: { " + "name = " + secondMusicPlayer.getName() + "; volume = " + secondMusicPlayer.getVolume() + " }");
 
-        System.out.println("musicPlayer.getName() = " + musicPlayer.getName());
-        System.out.println("musicPlayer.getVolume() = " + musicPlayer.getVolume());
+        // Считаем количество вызовов метода init класса ClassicalMusic
+        System.out.println("=".repeat(50));
+        for (int i = 0; i < new Random().nextInt(5); i++) {
+            context.getBean("musicClassic", ClassicalMusic.class);
+        }
+        System.out.println("COUNT_INIT = " + COUNT);
 
+        // Закрытие ApplicationContext
+        System.out.println("=".repeat(50));
         context.close();
+
     }
 }
